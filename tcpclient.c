359c422094c28ee90c3ccb78a09a7b64877adf45
Abdolrahim Roosta <rahim.ro@outlook.com>
2020-10-03 22:00:47 +0200

    adding tcpclient.c & tcpserver.c

---
+++ tcpclient.c
@@ -0,0 +1,316 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+
+void main(int argc, char *argv[]){
+	int clientSocket, n, m, portno, serverip, c;
+	struct sockaddr_in serverAddr;
+	char buffer [32];
+	char newbuffer[256];
+	char buffer2[256];
+	char str1[] = "TEXT TCP 1.0\n\n";
+	char str2[] = "OK\n";
+	int result, result1;
+	char delim[] = " ";
+	char *str3[3];
+	int i = 0;
+	int operation;
+	int num1, num2, cal2;
+	float num3, num4, cal1;
+	char fadd[] = "fadd";
+	char fdiv[] = "fdiv";
+	char fmul[] = "fmul";
+	char fsub[] = "fsub";
+	char add[] = "add";
+	char div[] = "div";
+	char mul[] = "mul";
+	char sub[] = "sub";
+	int flag = 0;
+	int length;
+	
+	int number_to_send = 2;
+	int convered_number = htonl(number_to_send);
+	
+
+	if (argc < 1)
+	{
+		printf("Please enter all arguments\n");
+		exit(0);
+	}
+
+//Split input arg (IP:PORT)
+	
+	char *ipv4, *port;
+	char* input = ":";
+	ipv4 = strtok(argv[1], input);
+	port = strtok(NULL, ":");
+	
+	portno = atoi(port);
+	printf("Ipv4: --%s--\n", ipv4);
+	printf("Port: --%d--\n", portno);
+
+//Creating socket	
+	clientSocket=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if(clientSocket<0)
+	{
+		printf("Client Socket Created Failed\n");
+		exit(0);
+	}	
+	printf("Client Socket Created Successfully\n");	
+	memset(&serverAddr, '\0', sizeof (serverAddr));
+	serverAddr.sin_family=AF_INET;
+	serverAddr.sin_port=htons(portno);
+	serverAddr.sin_addr.s_addr=inet_addr(ipv4);
+//Connect to the socket
+	c=connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if (c<0)
+	{
+		printf("Connection Error, server not responding!\n");
+		exit(0);
+	}
+	printf("Connect to the Server\n");
+	
+	
+	
+	
+	bzero(buffer,256);
+	m=recv(clientSocket, buffer, 256, 0);
+	printf("Version receaved from Server: %s\n", buffer);
+	result = strcmp(str1, buffer);	
+	printf("result: %d\n", result);
+	if (result == 0)
+	{
+		//bzero(buffer,256);
+		n = send(clientSocket, str2,strlen(str2),0);
+		
+		
+		bzero(buffer,256);
+		m=recv(clientSocket, buffer, sizeof(buffer), 0);
+		printf("Assignment from Server:-%s-\n", buffer);
+		
+		
+				//check the floating or integer
+		char *ptr1 = strstr(buffer, ".");
+		if( ptr1 != NULL)
+		{
+			printf("Floating\n");
+			//split the string(received data) to array
+			char *ptr = strtok(buffer, delim);
+			while (ptr != NULL)
+			{
+				str3[i++] = ptr;
+				ptr = strtok(NULL, delim);
+				//printf("%s\n", str3[i++]);
+			
+			}
+			for (i=0; i<3; ++i)
+			{			
+				printf("%s\n", str3[i]);
+			}
+			
+			//check the opearion type
+			if (strcmp(str3[0], fadd)==0)
+				{
+					num3 = atof (str3[1]);
+					num4 = atof (str3[2]);
+					cal1 = num3 + num4;
+					printf("In the loop FADD: %f\n", cal1);
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					snprintf(buffer,sizeof(buffer), "%f\n", cal1);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: ---%s---\n", buffer);
+					n=write(clientSocket, buffer, strlen(buffer));
+					printf("Buffer2222: ...%s...\n", buffer);
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					}
+			if (strcmp(str3[0], fdiv)==0)
+				{
+					num3 = atof (str3[1]);
+					num4 = atof (str3[2]);
+					cal1 = num3 / num4;
+					printf("In the loop FADD: %f\n", cal1);
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					snprintf(buffer,sizeof(buffer), "%f\n", cal1);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: ---%s---\n", buffer);
+					n=write(clientSocket, buffer, strlen(buffer));
+					printf("Buffer2222: ...%s...\n", buffer);
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					}
+			if (strcmp(str3[0], fmul)==0)
+				{
+					num3 = atof (str3[1]);
+					num4 = atof (str3[2]);
+					cal1 = num3 * num4;
+					printf("In the loop FADD: %f\n", cal1);
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					snprintf(buffer,sizeof(buffer), "%f\n", cal1);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: ---%s---\n", buffer);
+					n=write(clientSocket, buffer, strlen(buffer));
+					printf("Buffer2222: ...%s...\n", buffer);
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					}	
+				if (strcmp(str3[0], fsub)==0)
+					{
+						num3 = atof (str3[1]);
+						num4 = atof (str3[2]);
+						cal1 = num3 * num4;
+						printf("In the loop FADD: %f\n", cal1);
+						bzero(buffer,256);
+						//bzero(buffer2,256);
+						//printf("\nEnter answer: ");
+						snprintf(buffer,sizeof(buffer), "%f\n", cal1);	//int to buffer
+						//fgets(buffer, 256, stdin);
+						printf("Buffer: ---%s---\n", buffer);
+						n=write(clientSocket, buffer, strlen(buffer));
+						printf("Buffer2222: ...%s...\n", buffer);
+						
+						bzero(buffer,256);
+						m=recv(clientSocket,buffer,256,0);
+						printf("Buffer3: %s\n", buffer);
+						}						
+		}
+		else
+		{
+			
+			//split the string(received data) to array
+			char *ptr = strtok(buffer, delim);
+			while (ptr != NULL)
+			{
+				str3[i++] = ptr;
+				ptr = strtok(NULL, delim);
+				//printf("%s\n", str3[i++]);
+			
+			}
+			for (i=0; i<3; ++i)
+			{			
+				printf("%s\n", str3[i]);
+			}
+			
+			//check the opearion type
+			if (strcmp(str3[0], add)==0)
+				{
+					num1 = atoi (str3[1]);
+					num2 = atoi (str3[2]);
+				
+					cal2 = num1 + num2;
+					
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					sprintf(buffer, "%d\n", cal2);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: %s\n", buffer);
+					n=write(clientSocket, buffer, sizeof(int));
+					
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					
+					
+				}
+				
+			if (strcmp(str3[0], div)==0)
+				{
+					num1 = atoi (str3[1]);
+					num2 = atoi (str3[2]);
+				
+					cal2 = num1 / num2;
+					
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					sprintf(buffer, "%d\n", cal2);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: %s\n", buffer);
+					n=write(clientSocket, buffer, sizeof(int));
+					
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					
+					
+				}
+		
+			if (strcmp(str3[0], mul)==0)
+				{
+					num1 = atoi (str3[1]);
+					num2 = atoi (str3[2]);
+				
+					cal2 = num1 * num2;
+					
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					sprintf(buffer, "%d\n", cal2);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: %s\n", buffer);
+					n=write(clientSocket, buffer, sizeof(int));
+					
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					
+					
+				}
+			if (strcmp(str3[0], sub)==0)
+				{
+					num1 = atoi (str3[1]);
+					num2 = atoi (str3[2]);
+				
+					cal2 = num1 - num2;
+					
+					bzero(buffer,256);
+					//bzero(buffer2,256);
+					//printf("\nEnter answer: ");
+					sprintf(buffer, "%d\n", cal2);	//int to buffer
+					//fgets(buffer, 256, stdin);
+					printf("Buffer: %s\n", buffer);
+					n=write(clientSocket, buffer, sizeof(int));
+					
+					
+					bzero(buffer,256);
+					m=recv(clientSocket,buffer,256,0);
+					printf("Buffer3: %s\n", buffer);
+					
+					
+				}
+		
+		
+		}
+
+
+	}
+	else
+	{
+		printf("TCP version is not matching\n");
+		exit(0);
+	}
+}
+
---
+++ tcpserver.c
@@ -0,0 +1,412 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <strings.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define MY_PORT		9999
+#define MAX_BUF		1024
+
+int main(int argc, char *argv[])
+{   
+    int sockfd;
+    int portno;
+	struct sockaddr_in self;
+	char buffer[32];
+	char str1[] = "TEXT TCP 1.0\n\n";
+	char str[] = "OK\n";
+	char str3[] = "Error\n";
+	int m, n;
+	int result, result2, result3, result4;
+	char fadd[] = "fadd";
+	char fdiv[] = "fdiv";
+	char fmul[] = "fmul";
+	char fsub[] = "fsub";
+	char add[] = "add";
+	char div[] = "div";
+	char mul[] = "mul";
+	char sub[] = "sub";
+	int r1, r2, r3, r4, r5;
+	char *arith[]={"add","div","mul","sub"};
+	int ans1, ans2, ans3;
+	float ans4;
+	char *arith1[]={"fadd","fdiv","fmul","fsub"};
+	float r6, r7, r8, r9;
+	float f1 = 50.0;
+	float ans5, ans6, ans7;
+	
+	time_t t;
+
+//Split input-arg (IP:PORT)
+	
+	char *ipv4, *port;
+	char* input = ":";
+	ipv4 = strtok(argv[1], input);
+	port = strtok(NULL, ":");
+	
+	portno = atoi(port);
+	printf("Ipv4: --%s--\n", ipv4);
+	printf("Port: --%d--\n", portno);
+	
+	
+
+//Create streaming socket
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("Socket");
+		exit(errno);
+	}
+
+//Initialize address/port structure
+	bzero(&self, sizeof(self));
+	self.sin_family = AF_INET;
+	self.sin_port = htons(portno);
+	self.sin_addr.s_addr = inet_addr(ipv4);
+
+//Assign a port number to the socket
+    if (bind(sockfd, (struct sockaddr*)&self, sizeof(self)) != 0)
+	{
+		perror("socket:bind()");
+		exit(errno);
+	}
+
+//Make it a "listening socket". Limit to 20 connections
+	if (listen(sockfd, 20) != 0)
+	{
+		perror("socket:listen()");
+		exit(errno);
+	}
+
+//Server run continuously
+	while (1)
+	{	int clientfd;
+		struct sockaddr_in client_addr;
+		int addrlen=sizeof(client_addr);
+
+//accept an incomming connection
+		clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+		printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+//send TCP version to client		
+		m=send(clientfd, str1, sizeof(str1), 0);
+		
+		bzero(buffer,256);
+//get TCP confirmation from Client
+		n=recv(clientfd, buffer, sizeof(buffer), 0);
+		printf("Version cinfirmation from client: --%s--\n", buffer);
+		printf("str: --%s--\n", str);
+//check OK answer from Client	
+		result = strcmp( str, buffer);
+		printf("result of OK: %d\n", result);
+		
+		
+		if (result == 0)
+			{
+			
+				
+				printf("Version matched\n");
+			
+				srand(time(0));
+				//Select int or float 
+				r1= rand() % 1;		//random seed
+				printf("Integer or Float?: %d\n", r1);
+				
+				if (r1 == 0)	
+					{
+						
+						r2= rand() % 3;
+						printf("r2: %d\n", r2);
+						//check Ineger or float in random
+						if (r2 == 0)  //integer
+							{
+							printf("Integer\n");
+							r3= rand() % 100 + 1;
+							printf("r3:%d\n", r3);
+							
+								
+							r4= rand() % 100 + 1;
+							printf("r4: %d\n", r4);
+							
+							r5= rand() % 3;
+							printf("r5: %d\n", r5);
+							
+							//result = strcmp(arith[r5], add);
+							if (strcmp(arith[r5], add)== 0)
+								{
+								
+								sprintf(buffer, "%s %d %d\n",add,r3, r4 );
+								printf("buffer: %s\n", buffer);
+								m=send(clientfd, buffer, sizeof(buffer), 0);
+								bzero(buffer, 256);
+								n=recv(clientfd, buffer, sizeof(buffer), 0);
+								printf("buffer rec: %s\n", buffer);
+								//check the answer
+								ans1 = r3+r4;
+								printf("answer1: --%d--\n", ans1);
+								ans2 = atoi(buffer);
+								printf("answer2 from client: --%d--\n", ans2);
+								ans3 = ans1-ans2;
+								printf("answer3: --%d--\n", ans3);
+								bzero(buffer, 256);
+								if (ans3<0.0001)
+								{
+									m=send(clientfd, str, sizeof(str), 0);
+									printf("msg send to client: %s\n", str);
+								}
+								else
+								{
+									m=send(clientfd, str3, sizeof(str3), 0);
+									printf("msg send to client: %s\n", str3);
+								}
+							}
+								if (strcmp(arith[r5], div)== 0)
+								{
+									printf("DIV\n");
+									sprintf(buffer, "%s %d %d\n",div,r3, r4 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans1 = r3/r4;
+									printf("answer1: --%d--\n", ans1);
+									ans2 = atoi(buffer);
+									printf("answer2 from client: --%d--\n", ans2);
+									ans3 = ans1-ans2;
+									printf("answer3: --%d--\n", ans3);
+									bzero(buffer, 256);
+									if (ans3<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}
+								}
+								if (strcmp(arith[r5], mul)== 0)
+								{
+									printf("MUL\n");
+									sprintf(buffer, "%s %d %d\n",mul,r3, r4 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans1 = r3*r4;
+									printf("answer1: --%d--\n", ans1);
+									ans2 = atoi(buffer);
+									printf("answer2 from client: --%d--\n", ans2);
+									ans3 = ans1-ans2;
+									printf("answer3: --%d--\n", ans3);
+									bzero(buffer, 256);
+									if (ans3<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}
+								}
+								if (strcmp(arith[r5], sub)== 0)
+								{
+									printf("SUB\n");
+									sprintf(buffer, "%s %d %d\n",sub,r3, r4 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans1 = r3-r4;
+									printf("answer1: --%d--\n", ans1);
+									ans2 = atoi(buffer);
+									printf("answer2 from client: --%d--\n", ans2);
+									ans3 = ans1-ans2;
+									printf("answer3: --%d--\n", ans3);
+									bzero(buffer, 256);
+									if (ans3<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}
+								}
+							
+							}	
+						else
+							{
+								printf("Float\n");
+								printf("\n");
+								r6= (float)rand() /(float)(RAND_MAX) * f1;
+								printf("r6:%f\n", r6);
+								
+									
+								r7= (float)rand() /(float)(RAND_MAX) * f1;
+								printf("r7: %f\n", r7);
+								
+								r5= rand() % 3;
+								printf("r5: %d\n", r5);
+								printf("random operation: %s\n", arith1[r5]);
+								
+								if (strcmp(arith1[r5], fadd)== 0)
+								{
+									printf("FADDDDDDDDD\n");
+									sprintf(buffer, "%s %f %f\n",fadd,r6, r7 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans5 = r6+r7;
+									printf("answer5: %f\n", ans5);
+									ans6 = atof(buffer);
+									printf("answer6 recv from client: --%f--\n", ans6);
+									ans7 = ans5-ans6;
+									printf("answer7: --%f--\n", ans7);
+									bzero(buffer, 256);
+									if (ans7<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}	
+								}
+								if (strcmp(arith1[r5], fdiv)== 0)
+								{
+									printf("FDIVVVVV\n");
+									sprintf(buffer, "%s %f %f\n",fdiv,r6, r7 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans5 = r6/r7;
+									printf("answer5: %f\n", ans5);
+									ans6 = atof(buffer);
+									printf("answer6 recv from client: --%f--\n", ans6);
+									ans7 = ans5-ans6;
+									printf("answer7: --%f--\n", ans7);
+									bzero(buffer, 256);
+									if (ans7<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}	
+									
+								}
+								if (strcmp(arith1[r5], fmul)== 0)
+								{
+									printf("FMULLLLLL\n");
+									sprintf(buffer, "%s %f %f\n",fmul,r6, r7 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans5 = r6*r7;
+									printf("answer5: %f\n", ans5);
+									ans6 = atof(buffer);
+									printf("answer6 recv from client: --%f--\n", ans6);
+									ans7 = ans5-ans6;
+									printf("answer7: --%f--\n", ans7);
+									bzero(buffer, 256);
+									if (ans7<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}	
+									
+								}
+								if (strcmp(arith1[r5], fsub)== 0)
+								{
+									printf("FSUBBBBB\n");
+									sprintf(buffer, "%s %f %f\n",fsub,r6, r7 );
+									printf("buffer: %s\n", buffer);
+									m=send(clientfd, buffer, sizeof(buffer), 0);
+									bzero(buffer, 256);
+									n=recv(clientfd, buffer, sizeof(buffer), 0);
+									printf("buffer rec: %s\n", buffer);
+									//check the answer
+									ans5 = r6-r7;
+									printf("answer5: %f\n", ans5);
+									ans6 = atof(buffer);
+									printf("answer6 recv from client: --%f--\n", ans6);
+									ans7 = ans5-ans6;
+									printf("answer7: --%f--\n", ans7);
+									bzero(buffer, 256);
+									if (ans7<0.0001)
+									{
+										m=send(clientfd, str, sizeof(str), 0);
+										printf("msg send to client: %s\n", str);
+									}
+									else
+									{
+										m=send(clientfd, str3, sizeof(str3), 0);
+										printf("msg send to client: %s\n", str3);
+									}	
+									
+								}
+								
+								
+							}			
+							}
+		
+					
+				
+				
+				
+			}
+			else
+			{
+				printf("version is not matching\n");
+				m=send(clientfd, str3, sizeof(str3), 0);
+				exit(0);
+			}
+		/** Echo back the received data to the client */
+		//send(clientfd, buffer, recv(clientfd, buffer, MAX_BUF, 0), 0);
+
+		/** Close data connection */
+		close(clientfd);
+	}
+
+	/** Clean up */
+	close(sockfd);
+	return 0;
+}
